0de29a93b3e32c8985815147df21d6fc45fe3677
aaron <av8r88@gmail.com>
2022-10-12 17:00:58 -0400

    buggy complete

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0 ..< 10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
+    }
 
 }
 
